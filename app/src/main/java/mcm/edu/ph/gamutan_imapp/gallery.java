473e7c62a9423e343cb99136a1155d899f544981
adibrv <adibruv@gmail.com>
2021-01-12 12:41:35 +0800

    woa

---
+++ app/src/main/java/mcm/edu/ph/gamutan_imapp/gallery.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.gamutan_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery);
+    }
+}
---
+++ app/src/main/java/mcm/edu/ph/gamutan_imapp/pmscreen.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.gamutan_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pmscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pmscreen);
+    }
+}
